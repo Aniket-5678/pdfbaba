39f3d9c55a46f6674f506fa9e9a8c615e4b8894c
Aniket-5678 <149232787+Aniket-5678@users.noreply.github.com>
2025-01-09 18:37:23 +0530

    banner changes

--- client/src/components/pages/Banner.js
+++ client/src/components/pages/Banner.js
@@ -20,21 +20,26 @@ const data = [
 const Banner = () => {
   return (
     <Carousel
-      className='coursel-container'
-      autoPlay={true}
-      animation="slide"
-      indicators={false}
-      navButtonsAlwaysVisible={true}
-      cycleNavigation={true}
-      navButtonsProps={{
-        style: {
-          backgroundColor: 'white',
-          color: 'black',
-          height: '104px',
-          borderRadius: '0px',
-          fontSize: '0.9rem',
-          boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', /* Adding shadow to nav buttons */
-          transition: 'background-color 0.3s ease, color 0.3s ease' /* Smooth color transition */
+    className='coursel-container'
+    autoPlay={true}
+    animation="slide"
+    indicators={false}
+    navButtonsAlwaysVisible={true}
+    cycleNavigation={true}
+    navButtonsProps={{
+      style: {
+        backgroundColor: 'white',
+        color: 'black',
+        height: '50px', // Circle size
+        width: '50px', // Circle size
+        borderRadius: '50%', // Makes it circular
+        fontSize: '1.5rem', // Adjusted font size for the arrows
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', /* Shadow effect */
+        transition: 'background-color 0.3s ease, color 0.3s ease', /* Smooth color transition */
+        cursor: 'pointer'
         }
       }}
     >
