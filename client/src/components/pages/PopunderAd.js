981ec5c5c5d9f334522cc2ee03edd5967cb22d51
Aniket-5678 <149232787+Aniket-5678@users.noreply.github.com>
2025-03-02 15:49:12 +0530

    new update

--- client/src/components/pages/PopunderAd.js
+++ client/src/components/pages/PopunderAd.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const PopunderAd = () => {
-  const handleAdClick = () => {
+  useEffect(() => {
     const script = document.createElement("script");
     script.type = "text/javascript";
     script.src = "//pl25749045.effectiveratecpm.com/99/0c/d5/990cd5e49735ddf7ebeb3acad08da550.js";
     script.async = true;
     document.body.appendChild(script);
-  };
 
-  return (
-    <button onClick={handleAdClick} style={{ padding: "10px 20px", cursor: "pointer" }}>
-      Click to Open Popunder Ad
-    </button>
-  );
+    return () => {
+      document.body.removeChild(script); // Component unmount hone par script remove ho jayega
+    };
+  }, []); // useEffect sirf ek baar chalega jab component mount hoga
+
+  return null; // Yeh component UI me kuch render nahi karega
 };
 
 export default PopunderAd;
